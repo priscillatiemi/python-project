ca7b6e2714b20dab953d584b9d9d0034e4d2fd87
Priscilla Kawakami <prips.tiemi@gmail.com>
2024-02-09 19:55:36 -0300

    subida branch pyproject

---
+++ aula1.py
@@ -0,0 +1,13 @@
+print(12, 34,sep="/",end="\n#")
+print(5, 14,sep="/",end="\n")
+
+#Escape
+print("Priscilla \"Tiemi\"")
+# r -> exibe o caracter de escape.
+print(r"Priscilla \"Tiemi\"")
+
+#Geralmente nao se usa o escape. 
+#Quando queremos usar aspas, só invertemos as aspas inicias:
+print('Priscilla "Tiemi"')
+
+#a string ficou bem mais limpa dessa forma.
---
+++ aula2.py
@@ -0,0 +1,12 @@
+#Tipos int e float
+
+#Tipos inteiros:
+print(1)
+print(0)
+print(-1)
+
+#Float:
+print(1.1, 1.2)
+print(0.0)
+numero = 0.1
+print(type(numero))
